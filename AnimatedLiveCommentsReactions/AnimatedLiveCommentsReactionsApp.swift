20e446a459ca25d2a205fe8a546bf37f48aeae64
Amos Gyamfi <amosgyamfi.fi@gmail.com>
2021-11-06 02:43:57 +0200

    Initial Commit

---
+++ AnimatedLiveCommentsReactions/AnimatedLiveCommentsReactionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedLiveCommentsReactionsApp.swift
+//  AnimatedLiveCommentsReactions
+//
+//  Created by amos.gyamfi@getstream.io on 6.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedLiveCommentsReactionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
